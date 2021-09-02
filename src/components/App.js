83f3f8c4447037802cd000f683ee77a48f102a5a
satish1396 <satyanarayanbingi14@gmail.com>
2021-09-02 13:59:49 +0530

    fix problem

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  let [count,setcount] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+    <button id="click" onClick={()=>setcount({count:true})}>Click me</button>
+      {
+        count?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null
+      }
     </div>
   );
 }
